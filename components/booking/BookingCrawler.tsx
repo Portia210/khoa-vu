ac68c57c89c28c8f06b88fdfe674e95e66244b07
khoavn02 <khoavungoc02@gmail.com>
2024-01-07 16:15:20 +0700

    update ui

--- components/booking/BookingCrawler.tsx
+++ components/booking/BookingCrawler.tsx
@@ -1,12 +1,9 @@
-import { useBookingCrawlerStateStore } from "./useBookingCrawlerStateStore"
 
 const BookingCrawlerComponent = () => {
-  const { startAuthentication } = useBookingCrawlerStateStore()
   return (
     <>
       <button
-        className="p-3 bg-blue-300 w-full text-white font-bolder text-lg"
-        onClick={startAuthentication}>
+        className="p-3 bg-blue-300 w-full text-white font-bolder text-lg">
         Start Booking Crawler
       </button>
     </>
--- components/travelor/TravelorCrawler.tsx
+++ components/travelor/TravelorCrawler.tsx
@@ -1,12 +1,9 @@
-import { useTravelorCrawlerStateStore } from "./useTravelorCrawlerStateStore"
 
 const TravelorCrawlerComponent = () => {
-  const { startAuthentication } = useTravelorCrawlerStateStore()
   return (
     <>
       <button
-        className="p-3 bg-green-300 w-full text-white font-bolder text-lg"
-        onClick={startAuthentication}>
+        className="p-3 bg-green-300 w-full text-white font-bolder text-lg">
         Start Travelor Crawler
       </button>
     </>
