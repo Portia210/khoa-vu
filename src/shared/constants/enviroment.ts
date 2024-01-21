686bb35e3eb50fed70e713e3ea128e6d6f1488af
khoavn02 <khoavungoc02@gmail.com>
2024-01-22 00:27:53 +0700

    update code

--- src/shared/constants/enviroment.ts
+++ src/shared/constants/enviroment.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = process.env.TOURCRAWLER_UI_URL ||  'http://localhost:3001';
--- src/shared/utils/fetchProxy.ts
+++ src/shared/utils/fetchProxy.ts
@@ -1,6 +1,6 @@
 import { HttpsProxyAgent } from 'https-proxy-agent';
 
-const proxyUrl =
+const proxyUrl = process.env.PROXY_URL ||
   'http://brd-customer-hl_f1601859-zone-datacenter_proxy1:e0ggk3jpoeja@brd.superproxy.io:22225';
 console.log('proxyUrl', proxyUrl);
 export const proxyAgent = new HttpsProxyAgent(proxyUrl);
